66a14868246536fa89a0c85a15f27591d3faa28e
Kazuho Oku <kazuhooku@gmail.com>
2012-03-20 01:49:17 +0900

    lower-case the exported types

--- picostring.h
+++ picostring.h
@@ -6,39 +6,39 @@
 
 template <typename StringT> class picostring {
 public:
-  typedef typename StringT::value_type CharT;
-  typedef typename StringT::size_type SizeT;
+  typedef typename StringT::value_type char_type;
+  typedef typename StringT::size_type size_type;
 private:
   
   class SimpleS;
   
   struct BaseS {
-    const SizeT size_;
+    const size_type size_;
     mutable size_t refcnt_;
-    BaseS(SizeT size) : size_(size), refcnt_(0) {}
+    BaseS(size_type size) : size_(size), refcnt_(0) {}
     virtual ~BaseS() {}
     const BaseS* retain() const { refcnt_++; return this; }
     void release() const {
       if (refcnt_-- == 0) delete const_cast<BaseS*>(this);
     }
-    virtual CharT at(SizeT pos) const = 0;
-    virtual const BaseS* substr(SizeT pos, SizeT length) const = 0;
+    virtual char_type at(size_type pos) const = 0;
+    virtual const BaseS* substr(size_type pos, size_type length) const = 0;
     virtual const BaseS* append(const BaseS* s) const = 0;
     virtual const BaseS* append(const StringT& s) const = 0;
     virtual const SimpleS* flatten() const = 0;
-    virtual CharT* flatten(CharT* out) const = 0;
+    virtual char_type* flatten(char_type* out) const = 0;
   };
   
   struct SimpleS : public BaseS {
     const StringT s_;
-    const SizeT first_;
-    const SizeT last_;
-    SimpleS(const StringT& s, SizeT first, SizeT last)
+    const size_type first_;
+    const size_type last_;
+    SimpleS(const StringT& s, size_type first, size_type last)
       : BaseS(last - first), s_(s), first_(first), last_(last) {}
-    virtual CharT at(SizeT pos) const {
+    virtual char_type at(size_type pos) const {
       return s_[first_ + pos];
     }
-    virtual const BaseS* substr(SizeT pos, SizeT length) const {
+    virtual const BaseS* substr(size_type pos, size_type length) const {
       return new SimpleS(s_, first_ + pos, first_ + pos + length);
     }
     virtual const BaseS* append(const BaseS* s) const {
@@ -53,7 +53,7 @@ private:
       size_t length = last_ - first_;
       return new SimpleS(s_.substr(first_, length), 0, length);
     }
-    virtual CharT* flatten(CharT* out) const {
+    virtual char_type* flatten(char_type* out) const {
       std::copy(s_.begin() + first_, s_.begin() + last_, out);
       return out + last_ - first_;
     }
@@ -69,11 +69,11 @@ private:
       left_->release();
       right_->release();
     }
-    virtual CharT at(SizeT pos) const {
+    virtual char_type at(size_type pos) const {
       return pos < left_->size_
 	? left_->at(pos) : right_->at(pos - left_->size_);
     }
-    virtual const BaseS* substr(SizeT pos, SizeT length) const {
+    virtual const BaseS* substr(size_type pos, size_type length) const {
       if (pos < left_->size_) {
 	if (pos + length <= left_->size_) {
 	  return left_->substr(pos, length);
@@ -92,11 +92,11 @@ private:
       return new LinkS(this->retain(), new SimpleS(s, 0, s.size()));
     }
     virtual const SimpleS* flatten() const {
-      StringT s(this->size_, CharT());
+      StringT s(this->size_, char_type());
       flatten(&s[0]);
       return new SimpleS(s, 0, this->size_);
     }
-    virtual CharT* flatten(CharT* out) const {
+    virtual char_type* flatten(char_type* out) const {
       out = left_->flatten(out);
       out = right_->flatten(out);
       return out;
@@ -123,13 +123,13 @@ public:
     if (s_ != NULL) s_->release();
   }
   bool empty() const { return s_ == NULL; }
-  SizeT size() const { return s_ != NULL ? s_->size_ : 0; }
-  CharT at(SizeT pos) const {
+  size_type size() const { return s_ != NULL ? s_->size_ : 0; }
+  char_type at(size_type pos) const {
     assert(s_ != NULL);
     assert(pos < s_->size_);
     return s_->at(pos);
   }
-  picostring substr(SizeT pos, SizeT length) const {
+  picostring substr(size_type pos, size_type length) const {
     assert(pos + length <= s_->size_);
     if (length == 0)
       return picostring();
@@ -201,11 +201,11 @@ int main(int, char**)
   
   is(picostr().str(), string());
   ok(picostr().empty());
-  is(picostr().size(), (picostr::SizeT)0);
+  is(picostr().size(), (picostr::size_type)0);
 
   is(picostr("").str(), string());
   ok(picostr("").empty());
-  is(picostr("").size(), (picostr::SizeT)0);
+  is(picostr("").size(), (picostr::size_type)0);
   
   is(picostr("").append("abc").str(), string("abc"));
   is(picostr("abc").append("de").str(), string("abcde"));
@@ -213,7 +213,7 @@ int main(int, char**)
 
   picostr s = picostr("abc").append("de").append("f");
   ok(! s.empty());
-  is(s.size(), (picostr::SizeT)6);
+  is(s.size(), (picostr::size_type)6);
   is(s.at(0), 'a');
   is(s.at(1), 'b');
   is(s.at(2), 'c');
